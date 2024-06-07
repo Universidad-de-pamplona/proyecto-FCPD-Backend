a454aa09511130a6ff37ddddb0cd7690a78adeb9
diyoifa <dani12ca12@gmail.com>
2024-06-07 06:13:16 -0500

    🌱 Feat(model): create task model

--- src/main/java/com/touchIon/backend/model/Task.java
+++ src/main/java/com/touchIon/backend/model/Task.java
@@ -1,4 +1,46 @@
 package com.touchIon.backend.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.UUID;
+
+@Entity
+@Table(name = "tasks")
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Task {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private String status;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "team_id")
+    private Team team;
 }
