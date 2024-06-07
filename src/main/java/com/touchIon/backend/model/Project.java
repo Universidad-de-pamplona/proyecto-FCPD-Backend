0c0d212d8f60a7ccbae6bfd12093d19c08fb6e44
diyoifa <dani12ca12@gmail.com>
2024-06-07 06:11:43 -0500

    🌱 Feat project model

--- src/main/java/com/touchIon/backend/model/Project.java
+++ src/main/java/com/touchIon/backend/model/Project.java
@@ -1,4 +1,44 @@
 package com.touchIon.backend.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "projects")
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Project {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @Column(nullable = false)
+    private LocalDateTime createdAt;
+
+    @Column(nullable = false)
+    private LocalDateTime deadline;
+
+    @ManyToMany
+    @JoinTable(
+            name = "project_teams",
+            joinColumns = @JoinColumn(name = "project_id"),
+            inverseJoinColumns = @JoinColumn(name = "team_id")
+    )
+    private Set<Team> teams;
 }
