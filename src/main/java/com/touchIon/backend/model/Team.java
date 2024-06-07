7638696ab2351b18e0054f29fceac2d09c8792ad
diyoifa <dani12ca12@gmail.com>
2024-06-07 06:13:51 -0500

    🌱 Feat(model): create task model

--- src/main/java/com/touchIon/backend/model/Team.java
+++ src/main/java/com/touchIon/backend/model/Team.java
@@ -1,4 +1,43 @@
 package com.touchIon.backend.model;
 
+import jakarta.persistence.*;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "teams")
+@ToString
+@Getter
+@Setter
+@EqualsAndHashCode
 public class Team {
+
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private String description;
+
+    @ManyToMany(mappedBy = "teams")
+    private Set<Project> projects;
+
+    @ManyToMany
+    @JoinTable(
+            name = "team_users",
+            joinColumns = @JoinColumn(name = "team_id"),
+            inverseJoinColumns = @JoinColumn(name = "user_id")
+    )
+    private Set<User> users;
+
+    @OneToMany(mappedBy = "team", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Task> tasks;
 }
