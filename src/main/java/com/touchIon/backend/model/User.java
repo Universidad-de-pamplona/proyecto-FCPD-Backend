b599757680c551960303f099906f3aa8e83555ff
diyoifa <dani12ca12@gmail.com>
2024-06-07 06:14:32 -0500

    🌱 Feat(model): create task model

--- src/main/java/com/touchIon/backend/model/User.java
+++ src/main/java/com/touchIon/backend/model/User.java
@@ -1,11 +1,13 @@
 package com.touchIon.backend.model;
 
-
 import jakarta.persistence.*;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+
+import java.util.Set;
+import java.util.UUID;
 
 @Entity
 @Table(name = "users")
@@ -15,4 +17,19 @@ import lombok.ToString;
 @EqualsAndHashCode
 public class User {
 
+    @Id
+    @GeneratedValue
+    private UUID id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private Set<Task> tasks;
 }
