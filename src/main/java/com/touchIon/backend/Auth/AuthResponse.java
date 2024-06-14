0d9c64a6eb543c594a7354252fda71e1659a97f0
diyoifa <dani12ca12@gmail.com>
2024-06-14 17:11:16 -0500

    feat: add AuthResponse class for authentication response

---
+++ src/main/java/com/touchIon/backend/Auth/AuthResponse.java
@@ -0,0 +1,16 @@
+package com.touchIon.backend.Auth;
+
+import com.touchIon.backend.model.User;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class AuthResponse {
+    String token;
+    User user;
+}
