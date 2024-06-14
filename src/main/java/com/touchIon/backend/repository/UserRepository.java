8912778fa2f08ff5df1de869f9bf96623666aa85
diyoifa <dani12ca12@gmail.com>
2024-06-14 17:12:28 -0500

    chore: Update UserRepository with additional query method

--- src/main/java/com/touchIon/backend/repository/UserRepository.java
+++ src/main/java/com/touchIon/backend/repository/UserRepository.java
@@ -2,10 +2,16 @@ package com.touchIon.backend.repository;
 
 import com.touchIon.backend.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.util.List;
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UserRepository  extends  JpaRepository<User, UUID>{
+    Optional<User> findByEmail(String email);
+    Optional<User> findByUsername(String username);
 
+    @Query("SELECT u FROM User u JOIN FETCH u.tasks")
+    List<User> findAllWithTasks();
 }
