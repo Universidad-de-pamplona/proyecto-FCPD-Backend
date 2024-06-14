c859e3d43b831e7ae093f3aaf2a207744ee67028
diyoifa <dani12ca12@gmail.com>
2024-06-14 17:12:32 -0500

    chore: Update UserService with additional methods

--- src/main/java/com/touchIon/backend/service/UserService.java
+++ src/main/java/com/touchIon/backend/service/UserService.java
@@ -4,9 +4,11 @@ import com.touchIon.backend.model.User;
 import com.touchIon.backend.repository.UserRepository;
 import com.touchIon.backend.service.interfaces.UserInterface;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 
@@ -40,6 +42,20 @@ public class UserService implements UserInterface {
     @Override
     public void deleteById(UUID id) {
         userRepository.deleteById(id);
+    }
+
+    @Transactional(readOnly = true)
+    public User findByEmail(String email) {
+        return userRepository.findByEmail(email).orElseThrow(() -> new UsernameNotFoundException("User not found with email : " + email));
+    }
+
+    @Transactional(readOnly = true)
+    public User findUserById(UUID id){
+        return userRepository.findById(id).orElseThrow(() -> new UsernameNotFoundException("User not found with id : " + id));
+    }
+
+    public List<User> findAllUsers() {
+        return userRepository.findAllWithTasks();
     }
 
 }
