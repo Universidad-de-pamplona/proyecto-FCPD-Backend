15db26d86b1feec704fca8ece5fb637fa6db9848
diyoifa <dani12ca12@gmail.com>
2024-06-14 17:12:20 -0500

    chore: Remove unused Repository annotation

--- src/main/java/com/touchIon/backend/repository/TaskRepository.java
+++ src/main/java/com/touchIon/backend/repository/TaskRepository.java
@@ -2,7 +2,6 @@ package com.touchIon.backend.repository;
 
 import com.touchIon.backend.model.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 import java.util.UUID;
 
